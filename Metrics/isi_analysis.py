bf3630758a5ad0589fe0d75db33dd8ea541cd1e4
SamPinky <sampinky1999@gmail.com>
2021-03-09 22:38:06 +0000

    Added isi calculations

--- Metrics/isi_analysis.py
+++ Metrics/isi_analysis.py
@@ -1,7 +1,21 @@
 import scipy.stats as stats
+import numpy as np
 
 
 def fit_gamma_distribution(isi_values):
     fit_alpha, fit_loc, fit_beta = stats.gamma.fit(isi_values)
     print(fit_alpha, fit_loc, fit_beta)
+    return fit_alpha
+
+
+def calculate_cv(isi_values):
+    sd = np.std(isi_values)
+    mean = np.mean(isi_values)
+    return sd/mean
+
+
+
+
+
+
 
